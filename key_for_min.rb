554b544668efc561cd1e4b7830e66853b00287f2
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-24 20:23:43 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+answer = nil
+holding =1000000
+name_hash.each do |keys,value|
+  if value < holding
+    answer = keys
+    holding = value
+  end
+end
+answer
 
 end
